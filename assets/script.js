378bd54a80e244b3908fd1d8fceba17b51b3a859
Dylan Panepucci <dylanpanepucci@Mac.lan>
2024-11-03 09:59:40 -0500

    Added JS to HTML file

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+const topics = ['HMTL', 'CSS', 'Git', 'JavaScript'];
+for(let x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+}
+/*
+if (topic === 'HTML') {
+    console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+    console.log("Lets's study CSS!");
+} else if (topic === 'Git') {
+    console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+    console.log("Let's studuy JavaScript!");
+} else {
+    console.log('Please try again!');
+}
+*/
